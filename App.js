337abfdcbb731f58d8221233be6f651e00f6590f
Ji <gafrong@Jis-Mac-mini.local>
2022-10-28 09:26:13 +0900

    first use case of FlatList

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Screens
+import ProductContainer from './Screens/Products/ProductContainer';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductContainer />
     </View>
   );
 }
@@ -18,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+

4afeef48633aa5aa7cf13cce6c00ca6f27d80198
gafrong <gafrong@gmail.com>
2022-11-08 17:17:34 +0900

    userProfile from database is working

--- Context/actions/Auth.actions.js
+++ Context/actions/Auth.actions.js
@@ -47,7 +47,8 @@ export const getUserProfile = (id) => {
         },
     })
     .then((res) => res.json())
-    .then((data)=> console.log(data));
+    .then((data)=> console.log(data))
+    .catch((error) => console.log('getUserProfile error', error))
 }
 
 export const logoutUser = (dispatch) => {
--- Screens/User/UserProfile.js
+++ Screens/User/UserProfile.js
@@ -1,12 +1,83 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext, useState, useCallback, useEffect } from 'react';
+import { View, Text, ScrollView, StyleSheet, Button } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import axios from 'axios';
+import baseURL from '../../assets/common/baseUrl';
+
+import AuthGlobal from '../../Context/store/AuthGlobal';
+import { logoutUser } from '../../Context/actions/Auth.actions';
+
 
 const UserProfile = (props) => {
+    const context = useContext(AuthGlobal)
+    const [userProfile, setUserProfile] = useState()
+
+    useEffect(() => {
+        if(
+            context.stateUser.isAuthenticated === false ||
+            context.stateUser.isAuthenticated === null
+        ) {
+            props.navigation.navigate("Login")
+        }
+
+        AsyncStorage.getItem("jwt")
+            .then((res) => {
+                axios
+                    .get(`${baseURL}users/${context.stateUser.user.userId}`, {
+                        headers: { Authorization: `Bearer ${res}`},
+                    })
+                    .then((user) => setUserProfile(user.data))
+            })
+            .catch((error) => console.log('Whatch this error', error.response.data))
+
+        return () => {
+            setUserProfile();
+        }    
+
+
+    }, [context.stateUser.isAuthenticated])
     return(
-        <View>
-            <Text>User Profile Screen</Text>
-        </View>
+        <>
+            <ScrollView contentContainerStyle={styles.subContainer}>
+                <Text style={{fontSize:30}}>
+                    {userProfile ? userProfile.name : ""}
+                </Text>
+                <View style={{marginTop:20}}>
+                    <Text style={{margin:10}}>
+                        Email: {userProfile ? userProfile.email : ""}
+                    </Text>
+                    <Text style={{margin:10}}>
+                        Phone: {userProfile ? userProfile.phone : ""}
+                    </Text>
+                </View>
+                <View style={{marginTop:20}}>
+                    <Button title={"Sign Out"} onPress={() => {
+                        AsyncStorage.removeItem("jwt"),
+                        logoutUser(context.dispatch)
+                    }}/>
+                </View>
+            </ScrollView>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center"
+    },
+    subContainer: {
+        alignItems: "center",
+        marginTop: 60
+    },
+    order: {
+        marginTop: 20,
+        alignItems: "center",
+        marginBottom: 60
+    }
+})
+
 
 export default UserProfile;

c084a864e75524f2160c8c1633efb3a4f618fb89
gafrong <gafrong@gmail.com>
2022-11-08 10:41:18 +0900

    updated login page

--- Screens/User/Register.js
+++ Screens/User/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Register = (props) => {
+const Register = () => {
     return(
         <View>
             <Text>Register Screen</Text>

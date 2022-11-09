1775cc289dcf5ffe7a8cc63aaec63f96b278e99a
gafrong <gafrong@gmail.com>
2022-11-09 10:08:37 +0900

    useFocusEffect on Admin Products component to load up and set state data

--- Navigators/AdminNavigator.js
+++ Navigators/AdminNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Orders from '../Screens/Admin/Orders';
 import Products from '../Screens/Admin/Products';

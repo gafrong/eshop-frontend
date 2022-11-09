c67b2f04c812582d37a16f95fe88dc64b0e82f0e
gafrong <gafrong@gmail.com>
2022-11-09 09:34:45 +0900

    show / hide admin button depending on if user isAdmin true or false

---
+++ Navigators/AdminNavigator.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+import Orders from '../Screens/Admin/Orders';
+import Products from '../Screens/Admin/Products';
+import ProductForm from '../Screens/Admin/ProductForm';
+import Categories from '../Screens/Admin/Categories';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+    <Stack.Navigator>
+        <Stack.Screen 
+            name="Products"
+            component={Products}
+            options={{
+                title:"Products"
+            }}
+        />
+         <Stack.Screen 
+            name="Categories"
+            component={Categories}
+        />
+        <Stack.Screen 
+           name="Orders"
+           component={Orders}
+       />
+       <Stack.Screen 
+          name="ProductForm"
+          component={ProductForm}
+      />
+    </Stack.Navigator>
+}
+
+export default function AdminNavigator() {
+    return <MyStack />
+}
--- Navigators/Main.js
+++ Navigators/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -7,12 +7,17 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import HomeNavigator from "./HomeNavigator";
 import CartNavigator from "./CartNavigator";
 import UserNavigator from "./UserNavigator";
+import AdminNavigator from "./AdminNavigator";
 
 import CartIcon from "../Shared/CartIcon";
+import AuthGlobal from "../Context/store/AuthGlobal";
 
 const Tab = createBottomTabNavigator();
 
 const Main = () => {
+
+    const context = useContext(AuthGlobal)
+
     return (
         <Tab.Navigator 
             initialRouteName="Home"
@@ -53,9 +58,11 @@ const Main = () => {
                     )
                 }}
            />
-           <Tab.Screen 
+           
+           {context.stateUser.user.isAdmin == true ? (
+            <Tab.Screen 
                 name="Admin"
-                component={HomeNavigator}
+                component={AdminNavigator}
                 options={{
                     tabBarIcon: ({color}) => (
                         <Icon 
@@ -65,7 +72,9 @@ const Main = () => {
                         />
                     )
                 }}
-           />
+            />
+           ) : null }
+
            <Tab.Screen 
                 name="User"
                 component={UserNavigator}

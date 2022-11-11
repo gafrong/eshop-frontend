baf537467c0e6c6ee498b2aa626311e5db6d3c1b
gafrong <gafrong@gmail.com>
2022-11-11 13:22:55 +0900

    basic form scaffold

--- Screens/Admin/ProductForm.js
+++ Screens/Admin/ProductForm.js
@@ -1,12 +1,139 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from "react"
+import { View, Text, Image, StyleSheet, TouchableOpacity, Platform } from 'react-native';
+import SelectList from 'react-native-dropdown-select-list';
+import FormContainer from '../../Shared/Form/FormContainer';
+import Input from '../../Shared/Form/Input';
+import BoutiqButton from '../../Shared/StyledComponents/BoutiqButton';
+import Error from '../../Shared/Error'
+import Icon from "react-native-vector-icons/FontAwesome"
+import Toast from 'react-native-toast-message';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import baseURL from '../../assets/common/baseUrl';
+import axios from 'axios';
 
 const ProductForm = (props) => {
+
+    const [selectValue, setSelectValue] = useState(); // use this instead of pickerValue, setPickerValue
+    const [brand, setBrand] = useState();
+    const [name, setName] = useState();
+    const [price, setPrice] = useState();
+    const [description, setDescription] = useState();
+    const [image, setImage] = useState();
+    const [mainImage, setMainImage] = useState();
+    const [category, setCategory] = useState();
+    const [categories, setCategories] = useState([]);
+    const [token, setToken] = useState();
+    const [err, setError] = useState();
+    const [countInStock, setCountInStock] = useState();
+    const [rating, setRating] = useState(0);
+    const [isFeatured, setIsFeature] = useState(false);
+    const [richDescription, setRichDescription] = useState();
+    const [numReviews, setNumReviews] = useState(0);
+    const [item, setItem] = useState(null);
+
     return(
-        <View>
-            <Text>ProductForm Screen</Text>
-        </View>
+        <FormContainer>
+            <View style={styles.imageContainer}>
+               <Image style={styles.image} source={{uri: mainImage}}/>
+               <TouchableOpacity style={styles.imagePicker}>
+                   <Icon style={{ color: "white"}} name="camera"/>
+               </TouchableOpacity>
+           </View>
+           <View style={styles.label}>
+               <Text style={{ textDecorationLine: "underline"}}>Brand</Text>
+           </View>
+           <Input 
+            placeholder="Brand"
+            name="brand"
+            id="brand"
+            value={brand}
+            onChangeText={(text) => setBrand(text)}
+           />
+           <View style={styles.label}>
+               <Text style={{ textDecorationLine: "underline"}}>Name</Text>
+           </View>
+           <Input 
+            placeholder="Name"
+            name="name"
+            id="name"
+            value={name}
+            onChangeText={(text) => setName(text)}
+           />
+            <View style={styles.label}>
+               <Text style={{ textDecorationLine: "underline"}}>Price</Text>
+           </View>
+           <Input 
+            placeholder="Price"
+            name="price"
+            id="price"
+            value={price}
+            keyboardType={"numeric"}
+            onChangeText={(text) => setPrice(text)}
+           />
+            <View style={styles.label}>
+               <Text style={{ textDecorationLine: "underline"}}>Count in Stock</Text>
+           </View>
+           <Input 
+            placeholder="Stock"
+            name="stock"
+            id="stock"
+            value={countInStock}
+            keyboardType={"numeric"}
+            onChangeText={(text) => setCountInStock(text)}
+           />
+            <View style={styles.label}>
+               <Text style={{ textDecorationLine: "underline"}}>Description</Text>
+           </View>
+           <Input 
+            placeholder="Description"
+            name="description"
+            id="description"
+            value={description}
+            onChangeText={(text) => setDescription(text)}
+           />
+        </FormContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    label: {
+        width: "80%",
+        marginTop: 10
+    },
+    buttonContainer: {
+        width: "80%",
+        marginBottom: 80,
+        marginTop: 20,
+        alignItems: "center"
+    },
+    buttonText: {
+        color: "white"
+    },
+    imageContainer: {
+        width: 200,
+        height: 200,
+        borderStyle: "solid",
+        borderWidth: 8,
+        padding: 0,
+        justifyContent: "center",
+        borderRadius: 100,
+        borderColor: "#E0E0E0",
+        elevation: 10
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 100
+    },
+    imagePicker: {
+        position: "absolute",
+        right: 5,
+        bottom: 5,
+        backgroundColor: "grey",
+        padding: 8,
+        borderRadius: 100,
+        elevation: 20
+    }
+})
 
 export default ProductForm;
